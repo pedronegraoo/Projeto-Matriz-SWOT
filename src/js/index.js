ed528dda48649b4bf1c70c6e9b4e8cdefc8d33b3
Pedro Negrao <macedopedro117@yahoo.com.br>
2023-12-11 15:02:43 -0300

    Melhora do código, boas práticas

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,38 @@
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (inputForca.value !== "") {
+      lerInput("forca");
+    } else if (inputFraquezas.value !== "") {
+      lerInput("fraqueza");
+    } else if (inputOportunidades.value !== "") {
+      lerInput("oportunidade");
+    } else if (inputAmeacas.value !== "") {
+      lerInput("ameaca");
+    }
+  }
+});
+
 //FORÇAS
 const inputForca = document.querySelector(".input-forca");
 const btnForca = document.getElementById("btn-forca");
 const caixaForca = document.getElementById("caixa-forca");
 
-document.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    if (inputForca.value !== "") {
-      lerInputForca();
-    } else if (inputFraquezas.value !== "") {
-      lerInputFraquezas();
-    } else if (inputOportunidades.value !== "") {
-      lerInputOportunidades();
-    } else if (inputAmeacas.value !== "") {
-      lerInputAmeacas();
-    }
-  }
-});
-
 let armazenarForcas = [];
 
-btnForca.addEventListener("click", lerInputForca);
+// btnForca.addEventListener("click", lerInputForca);
+btnForca.addEventListener("click", () => {
+  lerInput("forca");
+});
 
-function lerInputForca() {
-  if (inputForca.value !== "") {
-    armazenarForcas.push(inputForca.value);
-    exibirNaTelaForca();
-    inputForca.value = "";
-  } else {
-    alert("Campo força vazio, preencha o campo.");
-  }
-}
-
-function exibirNaTelaForca() {
-  let novaLista = "";
-
-  armazenarForcas.forEach((mensagem, posicao) => {
-    novaLista =
-      novaLista +
-      `<li class="item-text">
-        <p>${mensagem}</p>
-        <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeForca(${posicao})">
-      </li>`;
-  });
-
-  caixaForca.innerHTML = novaLista;
-}
-
-function removeForca(posicao) {
-  armazenarForcas.splice(posicao, 1);
-  exibirNaTelaForca();
-}
+/* function lerInputForca() {
+//   if (inputForca.value !== "") {
+//     armazenarForcas.push(inputForca.value);
+//     exibirNaTela("forca");
+//     inputForca.value = "";
+//   } else {
+//     alert("Campo força vazio, preencha o campo.");
+//   }
+// }*/
 
 // FRAQUEZAS
 const inputFraquezas = document.querySelector(".input-fraquezas");
@@ -58,37 +41,9 @@ const caixaFraquezas = document.getElementById("caixa-fraquezas");
 
 let armazenarFraquezas = [];
 
-btnFraquezas.addEventListener("click", lerInputFraquezas);
-
-function lerInputFraquezas() {
-  if (inputFraquezas.value !== "") {
-    armazenarFraquezas.push(inputFraquezas.value);
-    exibirNaTelaFraquezas();
-    inputFraquezas.value = "";
-  } else {
-    alert("Campo fraquezas vazio, preencha o campo.");
-  }
-}
-
-function exibirNaTelaFraquezas() {
-  let novaLista = "";
-
-  armazenarFraquezas.forEach((mensagem, posicao) => {
-    novaLista =
-      novaLista +
-      `<li class="item-text">
-        <p>${mensagem}</p>
-        <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeFraqueza(${posicao})">
-      </li>`;
-  });
-
-  caixaFraquezas.innerHTML = novaLista;
-}
-
-function removeFraqueza(posicao) {
-  armazenarFraquezas.splice(posicao, 1);
-  exibirNaTelaFraquezas();
-}
+btnFraquezas.addEventListener("click", () => {
+  lerInput("fraqueza");
+});
 
 // OPORTUNIDADES
 const inputOportunidades = document.querySelector(".input-oportunidades");
@@ -97,37 +52,9 @@ const caixaOportunidades = document.getElementById("caixa-oportunidades");
 
 let armazenarOportunidades = [];
 
-btnOportunidades.addEventListener("click", lerInputOportunidades);
-
-function lerInputOportunidades() {
-  if (inputOportunidades.value !== "") {
-    armazenarOportunidades.push(inputOportunidades.value);
-    exibirNaTelaOportunidades();
-    inputOportunidades.value = "";
-  } else {
-    alert("Campo oportunidades vazio, preencha o campo.");
-  }
-}
-
-function exibirNaTelaOportunidades() {
-  let novaLista = "";
-
-  armazenarOportunidades.forEach((mensagem, posicao) => {
-    novaLista =
-      novaLista +
-      `<li class="item-text">
-        <p>${mensagem}</p>
-        <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeOportunidade(${posicao})">
-      </li>`;
-  });
-
-  caixaOportunidades.innerHTML = novaLista;
-}
-
-function removeOportunidade(posicao) {
-  armazenarOportunidades.splice(posicao, 1);
-  exibirNaTelaOportunidades();
-}
+btnOportunidades.addEventListener("click", () => {
+  lerInput("oportunidade");
+});
 
 // AMEAÇAS
 const inputAmeacas = document.querySelector(".input-ameacas");
@@ -136,34 +63,113 @@ const caixaAmeacas = document.getElementById("caixa-ameacas");
 
 let armazenarAmeacas = [];
 
-btnAmeacas.addEventListener("click", lerInputAmeacas);
+btnAmeacas.addEventListener("click", () => {
+  lerInput("ameaca");
+});
 
-function lerInputAmeacas() {
-  if (inputAmeacas.value !== "") {
-    armazenarAmeacas.push(inputAmeacas.value);
-    exibirNaTelaAmeacas();
-    inputAmeacas.value = "";
-  } else {
-    alert("Campo ameaças vazio, preencha o campo.");
+// LER INPUT'S
+function lerInput(input) {
+  if (input === "forca") {
+    if (inputForca.value !== "") {
+      armazenarForcas.push(inputForca.value);
+      exibirNaTela("forca");
+      inputForca.value = "";
+    } else {
+      alert("Campo força vazio, preencha o campo.");
+    }
+  } else if (input === "fraqueza") {
+    if (inputFraquezas.value !== "") {
+      armazenarFraquezas.push(inputFraquezas.value);
+      exibirNaTela("fraqueza");
+      inputFraquezas.value = "";
+    } else {
+      alert("Campo fraquezas vazio, preencha o campo.");
+    }
+  } else if (input === "oportunidade") {
+    if (inputOportunidades.value !== "") {
+      armazenarOportunidades.push(inputOportunidades.value);
+      exibirNaTela("oportunidade");
+      inputOportunidades.value = "";
+    } else {
+      alert("Campo oportunidades vazio, preencha o campo.");
+    }
+  } else if (input === "ameaca") {
+    if (inputAmeacas.value !== "") {
+      armazenarAmeacas.push(inputAmeacas.value);
+      exibirNaTela("ameaca");
+      inputAmeacas.value = "";
+    } else {
+      alert("Campo ameaças vazio, preencha o campo.");
+    }
   }
 }
 
-function exibirNaTelaAmeacas() {
+// FUNÇÃO PARA EXIBIR NA TELA
+function exibirNaTela(tipo) {
   let novaLista = "";
 
-  armazenarAmeacas.forEach((mensagem, posicao) => {
-    novaLista =
-      novaLista +
-      `<li class="item-text">
-        <p>${mensagem}</p>
-        <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeAmeaca(${posicao})">
-      </li>`;
-  });
+  console.log(tipo);
 
-  caixaAmeacas.innerHTML = novaLista;
+  if (tipo === "forca") {
+    armazenarForcas.forEach((mensagem, posicao) => {
+      novaLista =
+        novaLista +
+        `<li class="item-text">
+          <p>${mensagem}</p>
+          <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeElemento('forca',${posicao})">
+        </li>`;
+    });
+
+    caixaForca.innerHTML = novaLista;
+  } else if (tipo === "fraqueza") {
+    armazenarFraquezas.forEach((mensagem, posicao) => {
+      novaLista =
+        novaLista +
+        `<li class="item-text">
+          <p>${mensagem}</p>
+          <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeElemento('fraqueza',${posicao})">
+        </li>`;
+    });
+
+    caixaFraquezas.innerHTML = novaLista;
+  } else if (tipo === "oportunidade") {
+    armazenarOportunidades.forEach((mensagem, posicao) => {
+      novaLista =
+        novaLista +
+        `<li class="item-text">
+          <p>${mensagem}</p>
+          <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeElemento('oportunidade',${posicao})">
+        </li>`;
+    });
+
+    caixaOportunidades.innerHTML = novaLista;
+  } else if (tipo === "ameaca") {
+    armazenarAmeacas.forEach((mensagem, posicao) => {
+      novaLista =
+        novaLista +
+        `<li class="item-text">
+          <p>${mensagem}</p>
+          <img src="./src/img/icons/lixeira32.png" class="trash" alt="trash" onClick="removeElemento('ameaca',${posicao})">
+        </li>`;
+    });
+
+    caixaAmeacas.innerHTML = novaLista;
+  }
 }
 
-function removeAmeaca(posicao) {
-  armazenarAmeacas.splice(posicao, 1);
-  exibirNaTelaAmeacas();
+// EXCLUIR ELEMENTO
+function removeElemento(id, posicao) {
+  if (id === "forca") {
+    armazenarForcas.splice(posicao, 1);
+    exibirNaTela(`${id}`);
+  } else if (id === "fraqueza") {
+    armazenarFraquezas.splice(posicao, 1);
+    exibirNaTela(`${id}`);
+  } else if (id === "oportunidade") {
+    armazenarOportunidades.splice(posicao, 1);
+    exibirNaTela(`${id}`);
+  } else if (id === "ameaca") {
+    armazenarAmeacas.splice(posicao, 1);
+    exibirNaTela(`${id}`);
+  }
 }
